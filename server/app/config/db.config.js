b4f1afd0ed4e40f812273c87464d30dfd1e81226
ny-aina-solofo <nyainarandrianarisolo@gmail.com>
2025-03-10 18:12:03 +0300

    refactor(db.config) : nettoyage du fichier

--- server/app/config/db.config.js
+++ server/app/config/db.config.js
@@ -1,10 +1,7 @@
 const mongoose = require("mongoose");
 const dotenv = require('dotenv');
 dotenv.config();
-// const url = process.env.MONGODB_CONNECTION_STRING_DEV;
 const url = process.env.MONGODB_CONNECTION_STRING_PROD;
-// const url = "mongodb://127.0.0.1:27017/todolist"
-// const url = "mongodb+srv://mongodb:mongodb@ny-aina.avf6r.mongodb.net/todolist"
 
 mongoose
     .connect(url)
